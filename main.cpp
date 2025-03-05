565f7ecb0e9e8eb8811e59e65a912fd2926d784f
EVgeny26 <usenko.euvgeny@mail.ru>
2025-03-05 10:19:03 +0300

    пава

--- main.cpp
+++ main.cpp
@@ -144,53 +144,6 @@ int main(){
         }
     }while(sw);
 
-    // flats->loadFromFile();
-
-    // flats->addFlat(FLAT{number, address, square, floor, side, date_sale, price, sale});
-    // flats->addFlat(FLAT{++number, address, ++square, ++floor, side, date_sale, 9, sale});
-    // flats->addFlat(FLAT{++number, address, ++square, ++floor, side, date_sale, 5, sale});
-    // flats->addFlat(FLAT{++number, address, ++square, ++floor, side, date_sale, 3, SALE(FOR_WORKERS)});
-    // flats->addFlat(FLAT{++number, address, ++square, ++floor, side, date_sale, 8, sale});
-
-    // cout<<flats->getLenght()<<" len prelast"<<endl;
-
-
-    // flats->printAll();
-
-    // cout<<endl;
-    // for(int i=0;i<flats->getLenght(); i++) cout<<(*flats)[i];
-    // cout << "getFlatsForData\n";
-    // flats->getFlatsForData(date_sale);
-    // cout << "getFlatsForMonthAndSide\n";
-    // flats->getFlatsForMonthAndSide(date_sale.month, date_sale.year);
-    // cout << "getFlatsForFirstHalfYear\n";
-    // flats->getFlatsForFirstHalfYear(date_sale.year);
-    // cout << "getFlatsForMonthAndPrice\n";
-    // cout << flats->getFlatsForMonthAndPrice(date_sale.month, date_sale.year, price, '=') << endl;
-    // cout << "getFlatsWithSale\n";
-    // flats->getFlatsWithSale();
-    // cout << "getFlatsWithSaleOnFloor\n";
-    // flats->getFlatsWithSaleOnFloor(3);
-    // cout << "getPersentSaleFromFlatForMonth\n";
-    // cout << flats->getPersentSaleFromFlatForMonth(date_sale.month, date_sale.year) << endl;
-    
-    // flats->saveToFile();
-
-    // flats->printAll();
-
-
-
-    // FLAT temp = (*flats)[2];
-    // (*flats)[2] = (*flats)[2 + 1];
-    // (*flats)[2 + 1] = temp;
-
-    // cout<<endl<<endl;
-    // flats->printAll();
-
-    // flats->sorted(6,1);
-
-    // cout<<endl<<endl;
-    // flats->printAll();
     delete flats;
 
     return 0;
